9bec625b3aa7ef48d98a8d5c2bf8e43c5a961ead
Hivin Manju Sri <127749982+HivinManjuSri@users.noreply.github.com>
2024-05-11 18:14:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
     var form = document.querySelector('form');
-    
+
     form.addEventListener('submit', function(event) {
         var searchInput = document.getElementById('search').value;
-        
+
         if (searchInput.toLowerCase().includes('<script>alert(1)</script>')) {
-            alert('1');
+            alert('Potential XSS attack detected!');
             console.log("FLAG!@#$%WELL%^&DONE");
             event.preventDefault();
-            
+        } else if (searchInput.toLowerCase() === 'correct answer') {
+            var flagElement = document.createElement('p');
+            flagElement.textContent = "FLAG!@#$%WELL%^&DONE";
+            document.body.appendChild(flagElement);
         }
     });
 });
